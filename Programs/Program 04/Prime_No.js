0f591d2c60457e352acde6a2d870ab1d82d2d3a4
MuhammadSayemIjaz <75717799+MuhammadSayemIjaz@users.noreply.github.com>
2021-01-12 13:03:46 +0500

    Adding Program 04

---
+++ Programs/Program 04/Prime_No.js
@@ -0,0 +1,14 @@
+// Prime Number Identifier
+// Get input from the user , identify this number is perime or not.
+
+function Show(Number) {
+
+}
+// console.log("Enter number is = 20");
+// console.log("Enter number is = 3");
+// console.log("Enter number is = 14");
+// console.log("Enter number is = 17");
+Show(9);
+// Show(3);
+// Show(14);
+// Show(17);
