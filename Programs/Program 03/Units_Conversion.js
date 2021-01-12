b296ad21c7541a0d2fddd9923847648de9593c25
MuhammadSayemIjaz <75717799+MuhammadSayemIjaz@users.noreply.github.com>
2021-01-12 13:02:45 +0500

    Adding Program 03

---
+++ Programs/Program 03/Units_Conversion.js
@@ -0,0 +1,10 @@
+// Units Conversion 
+// Converting height in inches into centimeters
+
+function Show(height) {
+    let HeightInInches
+    HeightInInches = 2.54 * height;
+    console.log("Height in Centimeters= " , HeightInInches); 
+}
+console.log("Height in Inches = 20 ");
+Show(20);
