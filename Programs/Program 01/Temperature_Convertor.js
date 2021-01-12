34bd7f914b4d8a8c17bb86fe5c620461033d4cd4
MuhammadSayemIjaz <75717799+MuhammadSayemIjaz@users.noreply.github.com>
2021-01-12 12:59:13 +0500

    Java Script Programs

---
+++ Programs/Program 01/Temperature_Convertor.js
@@ -0,0 +1,9 @@
+// Temperature Convertor 
+// Convert Celcius into Far.
+
+function Temperature(C){
+    let F;
+    F = (9/5 * C) + 32;
+    console.log("TEMPERATURE :" , F);
+}
+Temperature(20.3);
